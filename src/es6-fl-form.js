f3ccf3627e2a631de90cc05c4c16ca5598ee9948
lazamar <lazaronijunior@gmail.com>
2016-05-12 19:26:02 +0100

    Add es6-fl-form

--- src/es6-fl-form.js
+++ src/es6-fl-form.js
@@ -1,5 +1,101 @@
-xController(function (xdiv) {
+/* globals xController */
 
-  
+import assert from '../node_modules/fl-assert/dist/assert.js';
 
+xController((xdiv) => {
+  assert(xdiv && xdiv.nodeName, 'Invalid x-div element given.');
+  const config = xdiv.dataset.config;
+  const flForm = new FlForm(xdiv, config); // eslint-disable-line no-unused-vars
 });
+
+class FlForm {
+  constructor(xdiv, config = {}) {
+    assert(xdiv && xdiv.nodeName, 'No x-div element provided.');
+    this.xdiv = xdiv;
+
+    // Apply configurations
+    this.config = config;
+    this.setConfigurations(config);
+
+    // Setup event listeners
+    this.setupEventListeners();
+
+    // trigger load event
+    this.config.onLoad(xdiv);
+  }
+
+  /**
+   * @method submitForm
+   * @param  {HTMLElement} form - The form element to be submitted.
+   * @return {Promise}  - A promise to be resolved with the response content
+   * 												when the form submission response arrive
+   * 												or with null if there is no submission URL.
+   */
+  submitForm(form, config = this.config) {
+    // Check that a valid element triggered the submit event.
+    assert(form.tagName === 'FORM', 'Submit event was fired without a form element.');
+
+    const url = form.getAttribute('action');
+    if (!url) { return Promise.resolve(null); }
+
+    // Prepare request options
+    const method = form.getAttribute('method') || 'GET';
+    const body = method.toUpperCase() === 'POST' ? new FormData(form) : undefined;
+    const fetchOptions = {
+      method,
+      body,
+      cache: 'default',
+      mode: config.mode,
+      credentials: config.credentials,
+    };
+
+    return fetch(url, fetchOptions);
+  }
+
+  processFormResponse(response, status) {
+    // If we have a response object rather than plain text,
+    // then come back when we have plain text.
+    if (response instanceof Response) {
+      response.text().then(text => {
+        this.processFormResponse(text, response.status);
+      });
+    }
+
+    // Render to the DOM
+    this.renderContent(response);
+
+    // Announce that we are finished.
+    this.config.onResponse(response, status, this.xdiv);
+  }
+
+  setConfigurations(config = this.config) {
+    assert(typeof config === 'object', 'Invalid configuration object');
+
+    this.config.onLoad = (typeof config.onLoad !== 'function')
+      ? () => true
+      : this.config.onLoad;
+
+    this.config.onResponse = (typeof config.onResponse !== 'function')
+      ? () => true
+      : this.config.onResponse;
+
+    this.config.credentials = config.credentials ? 'include' : '';
+    this.config.mode = config.credentials ? 'cors' : 'same-origin';
+  }
+
+  setupEventListeners(xdiv = this.xdiv) {
+    xdiv.addEventListener('submit', (e) => {
+      // Let's not allow the page to reload
+      e.preventDefault();
+
+      // Now we just submit and process the reponse
+      const form = e.target;
+      this.submitForm(form)
+        .then(res => this.processFormResponse(res));
+    });
+  }
+
+  renderContent(content, xdiv = this.xdiv) {
+    xdiv.innerHTML = content;
+  }
+}
